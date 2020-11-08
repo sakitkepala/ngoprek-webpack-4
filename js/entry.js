3fc374b3ef4fb032fe3083016bf38dc0625d9f38
sakitkepala <andikapriyotama@gmail.com>
2020-11-08 08:44:49 +0700

    Set up dependensi Bootstrap

--- js/entry.js
+++ js/entry.js
@@ -1,21 +1,21 @@
-import info from './info.json'
+import info from "./info.json";
 
-$('#logo').attr('src',info.logo)
-$('#title').text(info.title)
+import $ from "jquery";
+import "popper.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-$('#img').attr('src',info.image)
-$('.card-title').text(info.city)
-$('.card-text').text(info.description)
+$("#logo").attr("src", info.logo);
+$("#title").text(info.title);
 
-function append_video(){
-    var video = "<video id='video' controls='controls' src="+info.video+"/>"
-    var banner = "<div class='alert alert-primary' role='alert'>Check Out the Video!</div>"
-    $(banner).appendTo('#video_box')
-    $(video).appendTo('#video_box')
+$("#img").attr("src", info.image);
+$(".card-title").text(info.city);
+$(".card-text").text(info.description);
+
+function append_video() {
+  var video = "<video id='video' controls='controls' src=" + info.video + "/>";
+  var banner = "<div class='alert alert-primary' role='alert'>Check Out the Video!</div>";
+  $(banner).appendTo("#video_box");
+  $(video).appendTo("#video_box");
 }
-append_video()
-
-
-
-
-
+append_video();

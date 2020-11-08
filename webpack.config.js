2a535fc137c01c9efcad383d1f20de7a2eaec018
sakitkepala <andikapriyotama@gmail.com>
2020-11-08 09:44:49 +0700

    Config webpack dengan loader & plugin utama

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./js/entry.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.mp4$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          // Path video ini akan ada di dalam direktori dist
+          // yang sudah diset di path output di atas.
+          // Bundle dari entry.js yang posisinya udah di dalam dist
+          // akan mereferensi video ke path ini.
+          outputPath: "video/",
+        },
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "image/",
+          // publicPath: "./" // ?
+        },
+      },
+      {
+        test: /\.ico$/,
+        loader: "file-loader",
+        // outputPath tidak dinyatakan untuk favicon supaya
+        // dikeluarkannya di direktori dist langsung, mengikuti
+        // path di output di atas
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./template.html",
+      favicon: "./favicon.ico",
+    }),
+  ],
+};
